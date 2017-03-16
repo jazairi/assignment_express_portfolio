3a2c425892b91eb4661680467b6b9a4b116a22bc
Adam Jazairi <adamjazairi@gmail.com>
2017-03-16 17:33:22 -0400

    Update Project class with screenshot param

--- models/projects.js
+++ models/projects.js
@@ -1,15 +1,16 @@
 class Project {
-  constructor(name, repo, ...technologies) {
+  constructor(name, repo, screenshot, ...technologies) {
     this.name = name
     this.repo = repo
+    this.screenshot = screenshot
     this.technologies = technologies
   }
 }
 
 const projects = [
-  new Project("Async Node.js", "https://github.com/jazairi/assignment_async_nodejs", "JavaScript", "node.js"),
-  new Project("Hello Node.js", "https://github.com/jazairi/assignment_node_hello_world", "Javascript", "node.js"),
-  new Project("Node.js Server", "https://github.com/jazairi/assignment_build_a_nodejs_server", "Javascript", "node.js")
+  new Project("Async Node.js", "https://github.com/jazairi/assignment_async_nodejs", "../public/images/async-node.png", "JavaScript", "node.js"),
+  new Project("Hello Node.js", "https://github.com/jazairi/assignment_node_hello_world", "../public/images/hello-node.png", "Javascript", "node.js"),
+  new Project("Node.js Server", "https://github.com/jazairi/assignment_build_a_nodejs_server", "../public/images/node-server.png", "Javascript", "node.js")
 ]
 
 module.exports = {
